4ce092d8691cfe92e22d2973f928aef45bedc1dc
vngowri <vngowri@gmail.com>
2017-06-05 14:18:01 -0500

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,13 +2,58 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'annonymous'}, action)=> {    
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var stateDefault = {
+    name: 'annonymous',
+      hobbies: [],
+      movies: []
     
+};
+
+
+var reducer = (state = stateDefault, action)=> {    
     switch (action.type) {
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.name
+            };
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                ...state.hobbies, 
+                {
+                    id: nextHobbyId++, 
+                    hobby: action.hobby
+                }
+            ]
+            };
+    
+        case 'REMOVE_HOBBY':
+            return {
+                ...state,
+                hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)           
+            };    
+    
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                ...state.movies, 
+                {
+                    id: nextMovieId++, 
+                    title: action.title,
+                    genre: action.genre
+                }
+            ]
+            };
+
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state.movies.filter((movie) => movie.id !== action.id)
             };
         default:
             return state;
@@ -25,6 +70,8 @@ var unsubscribe = store.subscribe(() => {
     
     console.log('Name is', state.name);
     document.getElementById('app').innerHTML = state.name;
+    
+    console.log('New State', store.getState());
 });
 
 //unsubscribe();
@@ -34,10 +81,57 @@ store.dispatch({
     name: 'Gowri'
 });
 
-
+store.dispatch({
+    type:  'ADD_HOBBY',    
+    hobby: 'Running'
+});
 
 store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Mead'
 });
+
+store.dispatch({
+    type:  'ADD_HOBBY',    
+    hobby: 'Walking'
+});
+
+store.dispatch({
+    type:  'REMOVE_HOBBY',    
+    id: 1
+});
+
+store.dispatch({
+    type:  'ADD_MOVIE',    
+    title: 'Ghost',
+    genre: 'Thriller'
+});
+
+store.dispatch({
+    type:  'ADD_MOVIE',    
+    title: 'X-Ray',
+    genre: 'Sci-Fi'
+});
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Tila'
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 2
+});
+
+store.dispatch({
+    type:  'ADD_MOVIE',    
+    title: 'Strom',
+    genre: 'Drama'
+});
+
+store.dispatch({
+    type:  'ADD_HOBBY',    
+    hobby: 'Biking'
+});
+
 
